867ac37aa2855385b96a765aa9007c2ba1acaedd
juanivalle <juanignaciovalle18@gmail.com>
2023-05-16 19:12:17 -0300

    pruebas

--- js/home-club.js
+++ js/home-club.js
@@ -11,4 +11,8 @@ var swiper = new Swiper(".mySwiper", {
         slideShadows: true,
     },
     loop: true,
+    autoplay: {
+        delay: 3000, // Tiempo de espera entre cada transición de diapositiva en milisegundos
+        disableOnInteraction: false, // Permite la reproducción automática incluso cuando el usuario interactúa con el slider
+      },
 });
