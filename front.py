7c3fd86537627d9ef88703062e61f1a0975f7e05
charlybarrio <5757@holbertonschool.com>
2023-05-22 13:39:24 -0300

    update template

--- front.py
+++ front.py
@@ -7,6 +7,10 @@ app = Flask(__name__, static_folder='css')
 def home():
     return render_template('home.html')
 
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/login.html')
 def login():
     return render_template('login.html')
@@ -16,4 +20,4 @@ def home_club():
     return render_template('home-club.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
