dc598a73137fa2198522ab6f5a471eb9c6ae7eec
juanivalle <juanignaciovalle18@gmail.com>
2023-05-22 11:08:04 -0300

    flask pillo

---
+++ front.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='css')
+
+@app.route('/')
+@app.route('/home.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/home-club.html')
+def home_club():
+    return render_template('home-club.html')
+
+if __name__ == '__main__':
+    app.run()
